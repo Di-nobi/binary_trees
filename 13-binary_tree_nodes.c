15a2149cfa123f1ec62ff425f6f7e513dde17895
Dinobi <architectstan01@gmail.com>
2023-03-30 16:34:03 -0700

    Binary trees

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -13,11 +13,11 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 
 	size_t count = 0;
 
-	if (tree->left || tree->right)
+	if (tree)
 	{
-		count++;
+		count += (tree->left || tree->right) ? 1 : 0;
+		count += binary_tree_nodes(tree->left);
+		count += binary_tree_nodes(tree->right);
 	}
-	count += binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right);
-
 	return (count);
 }
