a6447a3d29557a20220b0196140ff7b32d28b24b
Dinobi <architectstan01@gmail.com>
2023-03-30 13:32:49 -0700

    Binary trees

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -7,10 +7,5 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	node = malloc(sizeof(binary_tree_t));
-
-	if (!node || node->parent)
-		return (0);
-
-	return (1);
+	return ((!node || node->parent) ? 0 : 1);
 }
